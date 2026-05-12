d915f643302456753cff92c1076432fcf202b9c6
elwelimedbabe <elwelimedbabe@icloud.com>
2026-05-12 02:30:39 +0000

    Add Product REST controller

---
+++ exo/src/main/java/com/tp1/exo/controller/ProductRestController.java
@@ -0,0 +1,57 @@
+package com.tp1.exo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tp1.exo.entity.Product;
+import com.tp1.exo.service.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductRestController {
+
+    private final ProductService productService;
+
+    public ProductRestController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.findById(id)
+                .orElseThrow(() ->
+                        new IllegalArgumentException("Invalid product id"));
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(
+            @PathVariable Long id,
+            @RequestBody Product product) {
+
+        product.setId(id);
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+}
